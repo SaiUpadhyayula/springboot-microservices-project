28cbfc7be2af3f2f80d49aea3a1400d4dace289c
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2021-05-29 15:42:16 +0200

    Added Endpoint to expose Session ID

--- api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
@@ -6,9 +6,17 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
+
+
+    @GetMapping("/")
+    public Mono<String> home(WebSession webSession) {
+        return Mono.just(webSession.getId());
+    }
 }
+
