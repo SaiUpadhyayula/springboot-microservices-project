29f18c610ae23a990cc057ec7e6e21716d307e44
Sai Upadhyayula <subramanyam.sai90@gmail.com>
2022-02-11 03:14:40 +0100

    Fix for Login Page bug

--- api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,6 +3,7 @@ package com.programming.techie.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -11,12 +12,6 @@ public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
-    }
-
-
-    @GetMapping("/")
-    public Mono<String> home(WebSession webSession) {
-        return Mono.just(webSession.getId());
     }
 }
 
--- api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/config/SecurityConfig.java
+++ api-gateway-service/api-gateway-service/src/main/java/com/programming/techie/apigatewayservice/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.programming.techie.apigatewayservice;
+package com.programming.techie.apigatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -6,17 +6,16 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-        http.authorizeExchange(exchanges -> exchanges.anyExchange().authenticated())
-                .oauth2Login(withDefaults());
-        http.csrf().disable();
-        return http.build();
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        serverHttpSecurity.authorizeExchange(exchange -> exchange.anyExchange().authenticated())
+                .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
+
+        serverHttpSecurity.csrf().disable();
+        return serverHttpSecurity.build();
     }
 }
